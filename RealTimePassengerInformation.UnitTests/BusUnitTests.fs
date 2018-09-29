96c7593b884887bd4f88d435547de3d51096af92
Matt Harrison <matt.anthony.harrison@gmail.com>
2018-09-29 20:15:28 +0100

    Adds unit test modules

--- RealTimePassengerInformation.UnitTests/BusUnitTests.fs
+++ RealTimePassengerInformation.UnitTests/BusUnitTests.fs
@@ -1,3 +1,25 @@
 ﻿namespace RealTimePassengerInformation.UnitTests
 
 open Xunit
+
+module Bus =
+    module BusStopInformation =
+        open RealTimePassengerInformation.Bus.BusStopInformation
+
+    module DailyTimeTableInformation =
+        open RealTimePassengerInformation.Bus.DailyTimeTableInformation
+
+    module FullTimeTableInformation =
+        open RealTimePassengerInformation.Bus.FullTimeTableInformation
+
+    module OperatorInformation =
+        open RealTimePassengerInformation.Bus.OperatorInformation
+
+    module RealTimeBusInformation =
+        open RealTimePassengerInformation.Bus.RealTimeBusInformation
+
+    module RouteInformation =
+        open RealTimePassengerInformation.Bus.RouteInformation
+
+    module RouteListInformation =
+        open RealTimePassengerInformation.Bus.RouteListInformation
